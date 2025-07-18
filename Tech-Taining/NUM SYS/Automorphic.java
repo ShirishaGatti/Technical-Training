d8a9c0892c1f87db0f4636c793f01b1556c05eb4
unknown <shirishagatti04@gmail.com>
2025-07-18 02:49:26 -0700

    Automorphic number

--- Tech-Taining/NUM SYS/Automorphic.java
+++ Tech-Taining/NUM SYS/Automorphic.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+// last digit of number and sq should be same 
 class Automorphic{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
