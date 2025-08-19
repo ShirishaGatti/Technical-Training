ae23d29ec5b91ad2d1a0d1d0f26ca87dc1668a86
ShirishaGatti <shirishagatti99@gmail.com>
2025-08-19 22:54:47 +0530

    POTD

---
+++ Tech-Taining/zeroFilledSubarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        int i=0,j=0;
+        long len=0;
+        while(j<nums.length){
+            if(nums[j]==0){
+                len+=(j-i+1);
+                j++;
+            }
+            else{
+                i=j+1;
+                j++;
+            }
+        }
+        return len;
+    }
+}
